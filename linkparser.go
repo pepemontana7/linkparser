9a82bd68cb7db36217999bee97f56ba6c9d79acd
Jose Delgado <jose.a.delgado.lopez@gmail.com>
2018-04-20 16:34:21 -0700

    version 1 of linke parser

--- linkparser.go
+++ linkparser.go
@@ -1,7 +1,12 @@
-package linkparser
+package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"golang.org/x/net/html"
 )
 
 type Link struct {
@@ -9,9 +14,45 @@ type Link struct {
 	Text string
 }
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func parseHTML(d io.Reader) ([]Link, error) {
+	var links []Link
+	doc, err := html.Parse(d)
+	check(err)
+	//ha := false
+	//var href string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val, n.FirstChild.Data)
+					links = append(links, Link{a.Val, n.FirstChild.Data})
+				}
+			}
+		} else if n.Type == html.ElementNode && n.Data == "/a" {
+			fmt.Println("here", n)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			fmt.Println(c)
+			f(c)
+		}
+	}
+	f(doc)
+	return links, nil
+}
 func main() {
 
-	htmlFile := flag.String("html", "ex1.html", "html to parse")
+	htmlFile := flag.String("html", "ex2.html", "html to parse")
 	flag.Parse()
-
+	htF, err := os.Open(*htmlFile)
+	check(err)
+	defer htF.Close()
+	links, err := parseHTML(htF)
+	fmt.Printf("%v", links)
 }
--- linkparser_test.go
+++ linkparser_test.go
@@ -1,24 +1,46 @@
-package linkparser
+package main
 
 import (
 	"os"
+	"reflect"
 	"testing"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 func TestLinkParserEx1(t *testing.T) {
 	// Create candidClient with cookieJAR
-	ht, err := os.Open("ex1.html")
+	htF, err := os.Open("ex1.html")
 	check(err)
-	defer ht.Close()
+	defer htF.Close()
+	link := Link{Href: "/other-page",
+		Text: "A link to another page"}
 	var links []Link
-	links, err = parseHTML(ht)
-	if len(links) != 1 {
-		t.Error("We should only see 1 link")
-	}
+	links, err = parseHTML(htF)
 
+	if len(links) != 1 {
+		t.Fatal("We should only see 1 link")
+	}
+	if link != links[0] {
+		t.Errorf("link do not match got %v expected %v", links[0], link)
+	}
+}
+
+func TestLinkParserEx2(t *testing.T) {
+	// Create candidClient with cookieJAR
+	htF, err := os.Open("ex2.html")
+	check(err)
+	defer htF.Close()
+	l1 := Link{Href: "https://www.twitter.com/joncalhoun",
+		Text: "Check me out on twitter"}
+	l2 := Link{Href: "https://github.com/gophercises",
+		Text: "Gophercises is on <strong>Github</strong>!"}
+	exLinks := []Link{l1, l2}
+	var links []Link
+	links, err = parseHTML(htF)
+
+	if len(links) != 2 {
+		t.Fatal("We should only see 2 link")
+	}
+	if !reflect.DeepEqual(exLinks, links) {
+		t.Errorf("link do not match got %v expected %v", exLinks, links)
+	}
 }
